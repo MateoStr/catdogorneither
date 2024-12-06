061c51d4f2f39afbf56cf3dcc1929e9a33bcc863
Mateo Strasdas <mateostrasdas@hotmail.com>
2024-12-06 09:49:18 -0800

    renamed existing default app compnonent into app_default for backup purposes, created new App component for working in

---
+++ frontend/CatDogOrNeither/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  test: string
+}>()
+</script>
+
+<template>
+    <h1>{{ test }}</h1>
+    <div id = "upload">test</div>
+    <div id = "result">test as well</div>
+</template>
+
+<style>
+    #upload{
+        min-width: 30%;
+    }
+    #result{
+        min-width: 30%;
+    }
+</style>
